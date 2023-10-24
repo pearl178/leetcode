9034237419388de7d27580c62db98c98a5824aa8
Pearl Sailikebuli <pearl@Pearls-MacBook-Pro.local>
2023-10-24 15:39:24 -0500

    q1 solved

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        dict = {}
+        for (i,num) in enumerate(nums):
+            if target-num not in dict:
+                dict[num]=i
+            else:
+                return [dict[target-num],i]
 # @lc code=end
 
